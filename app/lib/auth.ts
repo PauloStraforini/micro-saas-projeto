34cb80175b905540559d9b17b302a63b44390c83
Straforini <134074601+PauloStraforini@users.noreply.github.com>
2025-04-16 13:59:17 -0300

    🎁 Create Prisma

--- app/lib/auth.ts
+++ app/lib/auth.ts
@@ -1,7 +1,10 @@
 import NextAuth from "next-auth"
 import Google from "next-auth/providers/google"
+import { prisma } from "@/app/lib/prisma"
+import { PrismaAdapter } from "@auth/prisma-adapter"
  
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [Google],
-  callbacks: {}
+  callbacks: {},
+  adapter: PrismaAdapter(prisma),
 })
