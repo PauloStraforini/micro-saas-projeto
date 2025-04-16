03b2be06e0af9f9d549b89098b019209bce5080f
Straforini <134074601+PauloStraforini@users.noreply.github.com>
2025-04-16 13:48:12 -0300

    Add sign-out functionality to dashboard and refactor related code

--- app/project/dashboard/page.tsx
+++ app/project/dashboard/page.tsx
@@ -1,29 +1,36 @@
-import { handleAuth } from "@/app/actions/handle-auth";
+import { handleSingOut } from "@/app/actions/handle-singOut";
 import { auth } from "@/app/lib/auth";
 import { redirect } from "next/navigation";
 
 export default async function dashboard() {
-  const session = await auth()
+  const session = await auth();
 
-  if (!session) { 
-    redirect("/project/login")
+  if (!session) {
+    redirect("/project/login");
   }
 
   return (
     <div>
-    <main>
-      <h1>Dashboard</h1>
-      <p>This is the dashboard page.</p>
-      <p>{session?.user?.email ? session?.user.email: "Usuario não esta logado!!"} </p>
+      <main>
+        <h1>Dashboard</h1>
+        <p>This is the dashboard page.</p>
+        <p>
+          {session?.user?.email
+            ? session?.user.email
+            : "Usuario não esta logado!!"}{" "}
+        </p>
 
-      {session?.user?.email && (
-
-                <form action={handleAuth}>
-                  <button className="border-black border rounderd md px-3 cursor-pointer:true mt-3" type="submit">Sair do sistema</button>
-                </form>
-        )
-      }
-    </main>
+        {session?.user?.email && (
+          <form action={handleSingOut}>
+            <button
+              className="border-black border rounderd md px-3 cursor-pointer:true mt-3"
+              type="submit"
+            >
+              Sair do sistema
+            </button>
+          </form>
+        )}
+      </main>
     </div>
   );
 }
