11ad6a71f61de9429f8be06cbe2b05f0656bc887
PauloStraforini <paulostraforini@gmail.com>
2025-04-28 17:06:19 -0300

    feat: add Mercado Pago checkout button to payment page

--- app/project/pagamentos/page.tsx
+++ app/project/pagamentos/page.tsx
@@ -1,6 +1,7 @@
 
 "use client";
 import { useStripe } from "@/app/hooks/use-tripe";
+import useMercadoPago from "@/app/hooks/useMercadoPago";
 
 export default function Pagamentos() {
   const {
@@ -8,6 +9,8 @@ export default function Pagamentos() {
     createPaymentStripeCheckout,
     handleCreateStripePortal,
   } = useStripe();
+
+  const { createMercadoPagoChekout } = useMercadoPago();
 
   return (
     <div className="p-8 max-w-xl mx-auto">
@@ -46,6 +49,17 @@ export default function Pagamentos() {
           onClick={handleCreateStripePortal}
         >
           Criar Portal de Pagamentos
+        </button>
+
+         <button
+          className="bg-purple-600 text-white py-2 px-4 rounded hover:bg-purple-700 transition"
+          onClick={() => createMercadoPagoChekout({
+            testeId: "price_12345",
+            userEmail: "aaaaaaaa@teste.com"
+          })
+        }
+        >
+          Criar Pagamento Mercado Pago
         </button>
       </div>
     </div>
